2ccd6a2ed1ef5dac67093e4efac5b308e261b464
RaeenMr <117382072+RaeenMr@users.noreply.github.com>
2023-04-13 16:15:21 +0330

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,47 +1,43 @@
-int buff[8];
-int counter, GY;
-int blocks;
-int angle_ball, direction_ball, distance_ball , x_ball , y_ball;
-int shr, shl, shb, dif;
-int v=130;
-bool is_ball;
-int a = 0;
-#define Wall_Distance 2600
-#define x_robot 110 //117
-#define y_robot 175 //145
+// ALL RIGHT RESERVED FOR ATP
+int   buff[8];
+int   counter, GY;
+int   blocks;
+float K_P=0, K_I=0, K_D=0, Heading, last_Heading;
+int   angle_ball, direction_ball, distance_ball , x_ball , y_ball;
+int   shr, shl, shb, dif;
+int   shoot_sens,sensor[9];
+int   b=4000;
+bool  is_ball,Ball_In_Kicker = false;
+bool  LDR_F,LDR_B,LDR_R,LDR_L;
+int   LDR_SET_F = 0,LDR_SET_R = 0,LDR_SET_B = 0,LDR_SET_L = 0;
+int   LCD_Print_Mode = 0;
+bool  already_shooted = false;
+int   out_cnt = 0;
+int   last_time;
+int   v=255;
+int   a = 0;
+#define Walll_Distance 2540
+#define Wallr_Distance 2700
+#define back_Distance 1000
+#define LDR_Sensitivity 300
+/////////////////////ATRT 2
+// #define x_robot 105 
+// #define y_robot 165
+/////////////////////ATRT 1
+//#define x_robot 103
+//#define y_robot 159 
+/////////////////////Robot 1
+#define x_robot 133
+#define y_robot 161 
+/////////////////////Robot 2
+//#define x_robot 118
+//#define y_robot 145 
 
 void setup() {
   inti();
+  set_ldr();
 }
 
 void loop() {
-  sensors();
-  print_all();
-  out_sharp();
-  // moveangle(a);
-  // delay(10);
-  // a++;
-  // a %= 360;
-  if(is_ball){
-    if(distance_ball > 70) {
-      v = 250;
-      move(direction_ball);
-    }  
-    else{
-      v = 130;
-      if(direction_ball==0)        move(direction_ball);
-      else if(direction_ball==1)   move(direction_ball + 1);
-      else if(direction_ball==15)  move(direction_ball - 1);
-      else if(direction_ball <=3)  move(direction_ball + 2);
-      else if(direction_ball <=8)  move(direction_ball + 3);
-      else if(direction_ball <=13) move(direction_ball - 3);
-      else                         move(direction_ball - 2);
-    }
-  }
-
-  else {
-    v = 150;
-    if(shb < 1000)    motor(-v + dif, -v - dif, v - dif, v + dif);
-    else              motor(0, 0, 0, 0);
-  }
+  AI_2();
 }
